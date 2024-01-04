50d729279a0de1b607cf470487ff95d704888dae
DNCoding2112 <84671419+DNCoding2112@users.noreply.github.com>
2024-01-04 18:42:48 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("hint").onclick = displayHint;
+
+function displayHint() {
+  document.getElementById("hint").innerHTML += "The background image of the question depicts the album the quoted song is originally from.";
+}
